976b18fae01cf5af4ee888c2fafd34affa249b70
djiang2 <djiang2>
2018-05-30 15:14:06 -0400

    Game window opens

--- Game.java
+++ Game.java
@@ -1,17 +1,15 @@
-public class Game{
+
+import java.awt.Canvas;
+
+public class Game extends Canvas{
 
     public Game(){
 	new Window(1000, 563,"PlayersUnknownMaze", this);
 	
     }
     
-    public void tick(){
-    }
-
-    public void render(){
-    }
     
-    public static main(String[]args){
+    public static void main(String[]args){
 	new Game();
     }
 }
