0595a6a210b7d9176acb18d24c0ecee96cd1607e
djiang2 <djiang2>
2018-05-30 15:02:32 -0400

    Added constructor for Game

--- Game.java
+++ Game.java
@@ -1,6 +1,17 @@
 public class Game{
 
+    public Game(){
+	new Window(1000, 563,"PlayersUnknownMaze", this);
+	
+    }
+    
+    public void tick(){
+    }
 
-
-
+    public void render(){
+    }
+    
+    public static main(String[]args){
+	new Game();
+    }
 }
