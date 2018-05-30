bd23e0fa0d2ce696852872fbf6d720bd0392b6a2
anne-zhang <azhang14@stuy.edu>
2018-05-30 15:18:22 -0400

    gamepiece

--- GamePiece.java
+++ GamePiece.java
@@ -1,26 +1,48 @@
 public abstract class GamePiece{
-	int x,y;
-	float velX = 0, velY = 0;
+    int x,y;
+    float velX = 0, velY = 0;
 	
-	public GamePiece(int x, int y){
-		this.x = x;
-		this.y = y;
-	}
+    public GamePiece(int x, int y){
+	this.x = x;
+	this.y = y;
+	ID id;
+    }
+
+    public ID getId(){
+	return id;
+    }
+    
+    public int getX(){
+	return x;
+    }
 	
-	public int getX(){
-		return x;
-	}
+    public void setX(int x){
+	this.x = x;
+    }
 	
-	public void setX(int x){
-		this.x = x;
-	}
+    public int getY(){
+	return y;
+    }
 	
-	public int getY(){
-		return y;
-	}
-	
-	public void setY(int y){
-		this.y = y;
-	}
+    public void setY(int y){
+	this.y = y;
+    }
+
+    public getVelX(){
+	return velX;
+    }
+
+    public void setVelX(float velX){
+	this.velX = velX;
+    }
+
+    public float getVelY(){
+	return velY;
+    }
+
+    public void setVelY(float velY){
+	this.velY = velY;
+    }
+
 	
 }
