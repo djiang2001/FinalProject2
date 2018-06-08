d3ca2b522793055030e21eb6141e8e0b409d74c1
Anne Zhang <azhang14@stuy.edu>
2018-06-08 19:13:11 -0400

    started the skeleton for the chests/hp

---
+++ Chest.java
@@ -0,0 +1,21 @@
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Chest extends GamePiece{
+	public Chest(int x, int y, ID id){
+		super(x, y, id);
+	}
+	
+	public void tick(){
+	
+	}
+	
+	public void render(Graphics g){
+	
+	}
+	
+	public Rectangle getBounds() {
+		return null;
+	}
+}
+	
