5b27fb23980ab7024675240ebc5aa3b2d095bea2
djiang2001 <djiang2@stuy.edu>
2018-06-09 10:28:01 -0400

    Fixed camera

--- Camera.java
+++ Camera.java
@@ -12,11 +12,13 @@ public class Camera{
 	x+= ((piece.getX() - x) - 1000/2) * 0.05f;
 	y+= ((piece.getY() - y) - 563/2) * 0.05f;
 
-	//collision camera
+		//collision camera
 	if(x<= 0) x = 0;
-	if(x >= 1100) x = 1100;
+      	if(x >= 2200) x = 2200;
 	if(y <= 0) y = 0;
-	if(y >= 663) y = 663;
+	if(y >= 2650) y = 2650;
+	
+	
     }
 
     public float getX(){
