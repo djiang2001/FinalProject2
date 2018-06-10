a6d5d3054098e66988e6ac663526fbbad99bf6bb
Anne Zhang <azhang14@stuy.edu>
2018-06-09 21:46:28 -0400

    fixed the bounds for the enemies but IDK WHAT HAPPENED TO THE BULLETS

--- Enemy.java
+++ Enemy.java
@@ -25,15 +25,16 @@ public class Enemy extends GamePiece{
 	    GamePiece tempPiece = client.piece.get(i);
 	    
 
-		if(tempPiece.getId() == ID.Player){
-		    if(getDetectionRadius().intersects(tempPiece.getBounds())){
-			float dist = (float) Math.sqrt((tempPiece.getX()-x)*(tempPiece.getX()-x) + (tempPiece.getY()-y)*(tempPiece.getY()-y));
-			velX = (tempPiece.getX() - x) / dist;
-			velY = (tempPiece.getY() - x) / Math.abs(tempPiece.getY() - velY)/ dist;
+		if(tempPiece.getId() == ID.Block){
+		    if(getOuterBounds().intersects(tempPiece.getBounds())){
+			x += (velX * 5) * -1;
+			y += (velY * 5) * -1;
+			velX *=  -1;
+			velY *=  -1;
 		    }else
 			if(choice == 0){
-			    velX = (r.nextInt(5 - -5) + -5);
-			    velY = (r.nextInt(5 - -5) + -5);
+			    velX = (r.nextInt(4 - -4) + -4);
+			    velY = (r.nextInt(4 - -4) + -4);
 			}
 		}
 
@@ -41,17 +42,20 @@ public class Enemy extends GamePiece{
 	    
 	    if(tempPiece.getId() == ID.Sword){
 	    	if(getBounds().intersects(tempPiece.getBounds())){
-		    hp -= 20;
+		    hp -= 25;
 		    client.removePiece(tempPiece);
 		}
 	    }
 	}
-	if(hp <= 0){
+	/*if(hp <= 0){
 	    x = spawnX;
 	    y = spawnY;
 	    hp = 100;
 
-	};
+	}*/
+		if(hp <= 0){
+			client.removePiece(this);
+		}
     }
 
     public void render(Graphics g){
@@ -65,9 +69,5 @@ public class Enemy extends GamePiece{
 
     public Rectangle getOuterBounds(){
 	return new Rectangle(x-8, y - 8, 32, 32);
-    }
-
-    public Rectangle getDetectionRadius(){
-	return new Rectangle(x - 4, y - 4, 500,500);
     }
 }
